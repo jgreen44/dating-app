2b9c58cfd5c1015e2ee9385788de6c6e754d0bdb
jgreen <jgreen44@asu.edu>
2023-08-29 10:15:32 -0500

    finished lesson 3

---
+++ API/Program.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.MapControllers();
+
+app.Run();
