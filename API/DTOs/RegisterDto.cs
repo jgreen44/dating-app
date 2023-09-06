db0ff9fc1ed180e8387070c9bf56cb74535f47df
jgreen <jgreen44@asu.edu>
2023-09-06 18:17:24 -0500

    end of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,8 +4,9 @@ namespace API.DTOs;
 
 public class RegisterDto
 {
+    [Required] public string Username { get; set; }
+
     [Required]
-    public string Username { get; set; }
-    [Required]
+    [StringLength(8, MinimumLength = 4)]
     public string Password { get; set; }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from "../_services/account.service";
 import { logMessages } from "@angular-devkit/build-angular/src/tools/esbuild/utils";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-register',
@@ -11,13 +12,16 @@ export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor (private accountService: AccountService) {
+  constructor (private accountService: AccountService, private toastr: ToastrService) {
   }
 
   register () {
     this.accountService.register(this.model).subscribe({
       next: () => this.cancel(),
-      error: error => console.log(error)
+      error: error => {
+        this.toastr.error(error.error)
+        console.log(error.error)
+      }
     })
   }
 
